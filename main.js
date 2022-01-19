2f69564d5d21bd814550115c6c9ff90ad09c45c5
jorge <fabricio861125@gmail.com>
2022-01-19 16:59:41 -0300

    terminado

--- main.js
+++ main.js
@@ -25,6 +25,18 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  //codigo aqui..
+  // 1 recorrer el array cosasQueAprendimos[]
+  cosasQueAprendimos.forEach(cosa => {
+    // 2 crear el li (hijo)
+    const nuevoItem = document.createElement('li');
+    nuevoItem.innerText = cosa.tema;  //con innerText le agrego texto a esa lista
+    nuevoItem.className = cosa.class;
+    // 3 agregar el li al padre (ul)
+    document.querySelector('ul.lista').appendChild(nuevoItem); // busca al padre 'ul' con la clase "lista" y le ponemos la propiedad agregar hijo llamado "nuevoItem"
+  })
+
+}
 
 main();
